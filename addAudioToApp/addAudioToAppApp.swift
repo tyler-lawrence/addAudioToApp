3f1cd4fd4fb84a7990c321d7cc080e60950083d8
Tyler Lawrence <tyler.lawrence15@gmail.com>
2022-03-04 08:16:20 -0500

    Initial Commit

---
+++ addAudioToApp/addAudioToAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  addAudioToAppApp.swift
+//  addAudioToApp
+//
+//  Created by Tyler Lawrence on 3/4/22.
+//
+
+import SwiftUI
+
+@main
+struct addAudioToAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
