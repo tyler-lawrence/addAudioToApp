f480529583c307100ab0f572d9d9dd59a6f9c7e4
Tyler Lawrence <tyler.lawrence15@gmail.com>
2022-03-04 09:28:13 -0500

    documentation for background music

--- addAudioToApp/BackgroundMusic.swift
+++ addAudioToApp/BackgroundMusic.swift
@@ -10,6 +10,14 @@ import AVFAudio
 
 var backgroundAudioPlayer: AVAudioPlayer?
 
+/// plays a file looping indefinitely
+///
+/// - Parameters:
+///     - sound: name of the audio file in your xcode project
+///     - type: type of file for sound parameter (ex: "mp3")
+/// - Returns:
+///     - plays music if possible
+
 func startBackgroundMusic(sound: String, type: String) {
     if let bundle = Bundle.main.path(forResource: sound, ofType: type) {
         let backgroundMusic = NSURL(fileURLWithPath: bundle)
@@ -25,6 +33,7 @@ func startBackgroundMusic(sound: String, type: String) {
     }
 }
 
+/// stops the file associated with this audio player
 func stopBackgroundMusic() {
     guard let backgroundAudioPlayer = backgroundAudioPlayer else { return }
     backgroundAudioPlayer.stop()
